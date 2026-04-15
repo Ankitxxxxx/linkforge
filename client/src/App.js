0982dbbc36fa669ed99951ff76cdad88c39d1988
Ankitxxxxx <aj.vk1817@gmail.com>
2026-04-16 01:16:37 +0530

    Update API endpoint for URL shortening

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const handleShorten = async () => {
     try {
       const res = await axios.post(
-        "https://linkforge-nlbw.onrender.com/shorten",
+        "https://linkforge-nlbw.onrender.com/api/shorten",
         { originalUrl: url }
       );
       setShort(res.data.shortUrl);
