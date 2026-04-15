6890e79e635c4a14ca41368f06d4d993214c1886
Ankitxxxxx <aj.vk1817@gmail.com>
2026-04-15 23:57:00 +0530

    Implement URL shortening feature in App component

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [url, setUrl] = useState("");
+  const [short, setShort] = useState("");
+
+  const handleShorten = async () => {
+    try {
+      const res = await axios.post(
+        "https://linkforge-nlbw.onrender.com/api/shorten",
+        { originalUrl: url }
+      );
+      setShort(res.data.shortUrl);
+    } catch (err) {
+      alert("Error shortening URL");
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🔗 LinkForge</h1>
+
+      <input
+        placeholder="Enter URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleShorten}>Shorten</button>
+
+      <br /><br />
+
+      {short && <a href={short}>{short}</a>}
+    </div>
+  );
+}
+
+export default App;
